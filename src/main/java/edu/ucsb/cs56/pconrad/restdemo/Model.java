51de00ff5c649ef3aa10ff8adc1f0709aefe7fa2
Phill Conrad <pconrad@cs.ucsb.edu>
2018-09-04 18:48:10 -0700

    unwind stream in Model to prepare for replacing with MongoDB

--- src/main/java/edu/ucsb/cs56/pconrad/restdemo/Model.java
+++ src/main/java/edu/ucsb/cs56/pconrad/restdemo/Model.java
@@ -1,9 +1,11 @@
 package edu.ucsb.cs56.pconrad.restdemo;
 
+import java.util.Set;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 // In a real application you may want to use a DB, for this example we just store the posts in memory
 
@@ -13,17 +15,22 @@ public class Model {
     
     
     public int createPost(String title, String content, List categories){
-	int id = nextId++;
-	Post post = new Post();
-	post.setId(id);
-	post.setTitle(title);
-	post.setContent(content);
-	post.setCategories(categories);
-	posts.put(id, post);
-	return id;
+		int id = nextId++;
+		Post post = new Post();
+		post.setId(id);
+		post.setTitle(title);
+		post.setContent(content);
+		post.setCategories(categories);
+		posts.put(id, post);
+		return id;
     }
     
     public List getAllPosts(){
-	return posts.keySet().stream().sorted().map((id) -> posts.get(id)).collect(Collectors.toList());
+		Map<Integer, Post> thePosts = this.posts;
+		Set<Integer> keyset = thePosts.keySet();
+		Stream<Integer> stream=keyset.stream();
+		stream = stream.sorted();
+		Stream<Post> postsStream = stream.map((id)->posts.get(id));
+		return postsStream.collect(Collectors.toList());
     }
 }
